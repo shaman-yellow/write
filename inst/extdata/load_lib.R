b58c63b5e43b4f1b7c7b46fd9fbe2aaf3ed8cd5e
echo <echo@pop-os.localdomain>
2022-08-04 21:21:40 +0800

    load

---
+++ inst/extdata/load_lib.R
@@ -0,0 +1,9 @@
+library(usethis)
+library(devtools)
+library(dplyr)
+library(data.table)
+library(stringr)
+.wtool = "~/legend/write/R"
+.wdic = "~/Documents/dictionary"
+.wstory = "~/Documents/story"
+load_all(.wtool)
